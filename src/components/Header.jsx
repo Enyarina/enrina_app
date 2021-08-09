227f748005a3f5464d0d917f9d3defd5ba97749c
Enyarina <enrina050220005@gmail.com>
2021-08-10 00:14:31 +0300

    header, navbar, profile - 1 version

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Header(){
+    return <header className='header'>
+        <img
+            src='https://klike.net/uploads/posts/2020-07/1593935092_27.jpg'
+            />
+    </header>
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+function Profile(){
+    return <div className='content'>
+        <div>
+            <img src='https://35photo.pro/photos_main/415/2078997.jpg'></img>
+        </div>
+        <div>
+            ava+description
+        </div>
+        <div>
+            My posts
+            <div>
+                New posts
+            </div>
+        </div>
+        <div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    </div>
+}
+export default Profile;
